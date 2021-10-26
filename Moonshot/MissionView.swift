cfd166bc2ea454fd2c216e81d0cbb93fb2d56adf
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-26 12:10:04 -0700

    Fixed broken images. The mission icons now display properly throughout the app

--- Moonshot/MissionView.swift
+++ Moonshot/MissionView.swift
@@ -9,6 +9,30 @@ import SwiftUI
 
 struct MissionView: View {
     let mission: Mission
+    let astronauts: [CrewMember]
+    
+    struct CrewMember {
+        let role: String
+        let astronaut: Astronaut
+    }
+    
+    init(mission: Mission, astronauts: [Astronaut]) {
+        self.mission = mission
+        
+        var matches = [CrewMember]()
+        
+        for member in mission.crew {
+            if let match = astronauts.first(where: {$0.id == member.name}) {
+                matches.append(CrewMember(role: member.role, astronaut: match))
+            }
+            else{
+                fatalError("Missing \(member)")
+            }
+        }
+        
+        self.astronauts = matches
+    }
+    
     var body: some View {
         GeometryReader{ geometry in
             ScrollView(.vertical){
@@ -32,8 +56,9 @@ struct MissionView: View {
 
 struct MissionView_Previews: PreviewProvider {
     static let missions: [Mission] = Bundle.main.decode("missions.json")
+    static let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
     
     static var previews: some View {
-        MissionView(mission: missions[0])
+        MissionView(mission: missions[0], astronauts: astronauts)
     }
 }
