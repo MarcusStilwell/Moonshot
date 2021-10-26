a282a303f0a4ead54c6c77399052fdbf3709d9a7
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-26 12:45:44 -0700

    Changed navigation link destination to be the corresponding mission view page

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -7,14 +7,15 @@
 
 import SwiftUI
 
-let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
-let missions: [Mission] = Bundle.main.decode("missions.json")
-
 struct ContentView: View {
+    
+    let astronauts: [Astronaut] = Bundle.main.decode("astronauts.json")
+    let missions: [Mission] = Bundle.main.decode("missions.json")
+    
     var body: some View {
         NavigationView {
             List(missions) { mission in
-                NavigationLink(destination: Text("Detail view")) {
+                NavigationLink(destination: MissionView(mission: mission, astronauts: self.astronauts)) {
                     Image(mission.image)
                         .resizable()
                         .aspectRatio(contentMode: .fit)
