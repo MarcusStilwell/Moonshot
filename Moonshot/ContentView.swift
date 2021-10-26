a65160f75506fbd203c517d486b7aec59f444770
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-25 19:59:15 -0700

    Initial Commit

---
+++ Moonshot/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Moonshot
+//
+//  Created by Marcus Stilwell on 10/25/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("Example")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
