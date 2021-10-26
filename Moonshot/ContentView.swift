53ec61f6d643461b96e2841aa9e5cdf6fff9da28
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-25 20:05:27 -0700

    Added image that automatically resizes using geometryreader

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -10,7 +10,12 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image("Example")
+            GeometryReader { geo in
+                Image("Example")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: geo.size.width)
+            }
         }
     }
 }
