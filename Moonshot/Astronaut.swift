cc0c6e4bba020491f4c3a2ad7257229c34e79d7a
MarcusStilwell <marcusstilwell@icloud.com>
2021-10-25 20:38:13 -0700

    Added JSON data, a decoder, and Added the path from the JSON to the Content View

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  Moonshot
+//
+//  Created by Marcus Stilwell on 10/25/21.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
